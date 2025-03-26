dd6a2fde673a935f8a40b9a0be0ab58ff32d273c
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-03-26 14:59:18 -0700

    Player rendered and can move

--- main.py
+++ main.py
@@ -2,19 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
-
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import *
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt = dt / 1000
 if __name__ == "__main__":
     main()
